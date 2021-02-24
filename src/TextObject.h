d26aa3b77d832ee2099c28ae33b547b3681df8e5
christiansencq <christiansencq@gmail.com>
2021-02-24 13:23:50 -0500

    Font fix.

--- src/TextObject.h
+++ src/TextObject.h
@@ -4,9 +4,9 @@
 #include <cmath>
 #include <stdio.h>
 #include <memory>
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
-#include "SDL2/SDL_ttf.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 class TextObject 
 {
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "SDL2/SDL.h"
+#include <SDL2/SDL.h>
 #include "App.h"
 #include "Constants.h"
 
